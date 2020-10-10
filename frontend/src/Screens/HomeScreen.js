55969e9b8c9cdb6ca09d114a24ea0c812f6b5433
Brian Bantigue <bignatue@gmail.com>
2020-10-10 12:24:36 -0400

    ProShop setup

---
+++ frontend/src/Screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Row, Col}  from  'react-bootstrap'
+import Product from '../Components/Product'
+import products from '../Products'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h2>Latest Products</h2>
+            <Row>
+                {products.map((product) => (
+                    <Col key={product._id} sm={12} md={6} xl={3}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    )
+}
+
+export default HomeScreen
