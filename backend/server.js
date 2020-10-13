bbbb7f9d76c8612a0f7912caf9628712d6eb1ee3
Brian Bantigue <bignatue@gmail.com>
2020-10-12 21:45:24 -0400

    Fetching products from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './Config/Db.js'
-import products from './Data/Products.js'
+import ProductRoutes from './Routes/ProductRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -13,14 +13,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
-
+app.use('/api/products', ProductRoutes)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
